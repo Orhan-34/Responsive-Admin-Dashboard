7e52560d49c984ad23bfa5848fb8fbc36834bb47
Orhan Özçelik <orhanozcelik3437@gmail.com>
2022-03-15 11:59:02 +0300

    files added and project finished

---
+++ README.md
@@ -0,0 +1,4 @@
+## Responsive Admin Dashboard | Light-Dark Mode
+
+### Dashboard | Light-Mode
+![Markdown Page]()
---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '3778',
+        paymentStatuts:'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '1992',
+        paymentStatuts:'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '1993',
+        paymentStatuts:'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '2008',
+        paymentStatuts:'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '3454',
+        paymentStatuts:'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '3428',
+        paymentStatuts:'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '7854',
+        paymentStatuts:'Paid',
+        shipping: 'Delivered'
+    }
+]
